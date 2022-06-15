3e30078176a8056b500913a2b955f98bb0c726fd
mitsuo adachi <aphxtwins320@gmail.com>
2022-06-16 05:25:21 +0900

    220616 手順６キャラクターの生成と生成位置制限の制御

---
+++ Assets/Scripts/CharaGenerator.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CharaGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject charaPrefab; //キャラプレファブの登録用
+
+    [SerializeField]
+    private Grid grid;　//タイルマップの座標を取得するための情報。Grid_Base側のGridを指定する。
+
+    [SerializeField]
+    private Tilemap tileMaps; //Walk 側の Tilemap を指定する
+
+    private Vector3Int gridPos; //タイルマップのタイルのセル座標の保持用
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO 配置できる最大キャラ数に達している場合には配置できない
+
+        // 画面をタップ(マウスクリック)したら
+        if (Input.GetMouseButtonDown(0))
+        {
+            // タップ(マウスクリック)の位置を取得してワールド座標に変換し、それをさらにタイルのセル座標に変換
+            gridPos = grid.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+            // タップした位置のタイルのコライダーの情報を確認し、それが None であるなら
+            if (tileMaps.GetColliderType(gridPos) == Tile.ColliderType.None)
+            {
+                // キャラ生成処理をメソッド化
+                CreateChara(gridPos);
+
+                // TODO 配置キャラ選択用ポップアップの表示
+            }
+        }
+    }
+
+    /// <summary>
+    /// キャラ生成
+    /// </summary>
+    /// <param name="gridPos"></param>
+    private void CreateChara(Vector3Int gridPos)
+    {
+        // タップした位置にキャラを生成して配置
+        GameObject chara = Instantiate(charaPrefab, gridPos, Quaternion.identity);
+
+        // キャラの位置がタイルの左下を 0,0 として生成しているので、タイルの中央にくるように位置を調整
+        chara.transform.position = new Vector2(chara.transform.position.x + 0.5f, chara.transform.position.y + 0.5f);
+
+    }
+
+}
